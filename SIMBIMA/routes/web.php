0c7151da3d1fba8084ffb79ca63c42bd82fc9614
MuhammadFarhan009 <farhan_m1@mhs.unsyiah.ac.id>
2023-05-18 13:35:47 +0700

    Fix: Routes

--- SIMBIMA/routes/web.php
+++ SIMBIMA/routes/web.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\dataController;
 
 
 
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -28,18 +30,19 @@ Route::get('/', function () {
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 
 Route::post('/login', [LoginController::class, 'login']);
-// Route ini akan mengarahkan ke halaman logout
-Route::get('/logout', [LoginController::class, 'destroy']);
-
-Route::resource("data_bimbingan", App\Http\Controllers\dataController::class);
-
-
 Route::get('/barchart/Data', function(){
     $data = DB::table('data_bimbingan')->get();
     return response()->json($data);
 
 });
+Route::resource("data_bimbingan", App\Http\Controllers\dataController::class);
 
 Route::get('/barchart', function(){
     return view('koordinator.barchart');
 });
+// Jika session login tidak ada maka akan diarahkan ke halaman login page dan jika ada maka akan diarahkan ke halaman dashboard
+Route::get('/dashboard', [PagesController::class, 'dashboard'])->middleware('auth');
+
+// Route ini akan mengarahkan ke halaman logout
+Route::get('/logout', [LoginController::class, 'destroy']);
+
